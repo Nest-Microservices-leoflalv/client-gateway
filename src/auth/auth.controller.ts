c63b382f9789f5e06bf0c57c5dbfd2e044d01e31
leonardo fleitas <leoflalv.dev@gmail.com>
2025-02-17 23:27:36 +0100

    add auth dtos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,20 @@
-import { Controller, Get, Inject, Post } from '@nestjs/common';
+import { Body, Controller, Get, Inject, Post } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { NATS_SERVICE } from 'src/config';
+import { LoginUserDto, RegisterUserDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(@Inject(NATS_SERVICE) private readonly client: ClientProxy) {}
 
   @Post('register')
-  registerUser(): unknown {
-    return this.client.send('auth.register.user', {});
+  registerUser(@Body() regiterUserDto: RegisterUserDto): unknown {
+    return this.client.send('auth.register.user', regiterUserDto);
   }
 
   @Post('login')
-  loginUser(): unknown {
-    return this.client.send('auth.login.user', {});
+  loginUser(@Body() loginUserDto: LoginUserDto): unknown {
+    return this.client.send('auth.login.user', loginUserDto);
   }
 
   @Get('verify')
